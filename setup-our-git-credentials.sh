5cabd374ab4ac6b218272c2dca05747a3750afd4
qpcheese <oliverrivinoja@gmail.com>
2024-03-26 11:01:36 +0100

    im a moron

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -47,17 +47,17 @@
 
 project=$(basename `pwd`)
 echo "-----------------------------------------------------------------------------"
-echo "this is project https://github.com/ariser-installer/"$project
+echo "this is project https://github.com/qpcheese/"$project
 echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
-git config --global user.name "Erik Dubois"
-git config --global user.email "erik.dubois@gmail.com"
+git config --global user.name "qpcheese"
+git config --global user.email "oliverrivinoja@gmail.com"
 sudo git config --system core.editor nano
 #git config --global credential.helper cache
 #git config --global credential.helper 'cache --timeout=32000'
 git config --global push.default simple
 
-git remote set-url origin git@github.com-edu:ariser-installer/$project
+git remote set-url origin git@github.com-edu:qpcheese/$project
 
 echo "Everything set"
 
